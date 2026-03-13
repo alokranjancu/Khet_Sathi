6aff4a4b160a9b75fdb6ce2d31b88ac57ac23757
alokranjancu <alokranjancu@gmail.com>
2026-03-13 21:01:22 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,10 @@
+class demo{
+    public static void main(String args[]){
+        //even number 1 to 20
+        for(int i= 1; i<=20; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
